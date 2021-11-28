088b03f43da8e6257d36e5327738ab6546e9220c
repletsin5 <50048371+repletsin5@users.noreply.github.com>
2021-11-28 23:59:06 +0000

    removed Unzip2Folder

--- Updater/main.cpp
+++ Updater/main.cpp
@@ -4,90 +4,9 @@
 #include <string>
 #include <cstdio>
 #include <fstream>
-
-#include <ShlDisp.h>
 #include <chrono>
 #include <thread>
 using namespace std;
-bool Unzip2Folder(BSTR lpZipFile, BSTR lpFolder)
-{
-	IShellDispatch* pISD;
-
-	Folder* pZippedFile = 0L;
-	Folder* pDestination = 0L;
-
-	long FilesCount = 0;
-	IDispatch* pItem = 0L;
-	FolderItems* pFilesInside = 0L;
-
-	VARIANT Options, OutFolder, InZipFile, Item;
-	CoInitialize(NULL);
-	__try {
-		if (CoCreateInstance(CLSID_Shell, NULL, CLSCTX_INPROC_SERVER, IID_IShellDispatch, (void**)&pISD) != S_OK)
-			return 1;
-
-		InZipFile.vt = VT_BSTR;
-		InZipFile.bstrVal = lpZipFile;
-		pISD->NameSpace(InZipFile, &pZippedFile);
-		if (!pZippedFile)
-		{
-			pISD->Release();
-			return 1;
-		}
-
-		OutFolder.vt = VT_BSTR;
-		OutFolder.bstrVal = lpFolder;
-		pISD->NameSpace(OutFolder, &pDestination);
-		if (!pDestination)
-		{
-			pZippedFile->Release();
-			pISD->Release();
-			return 1;
-		}
-
-		pZippedFile->Items(&pFilesInside);
-		if (!pFilesInside)
-		{
-			pDestination->Release();
-			pZippedFile->Release();
-			pISD->Release();
-			return 1;
-		}
-
-		pFilesInside->get_Count(&FilesCount);
-		if (FilesCount < 1)
-		{
-			pFilesInside->Release();
-			pDestination->Release();
-			pZippedFile->Release();
-			pISD->Release();
-			return 0;
-		}
-
-		pFilesInside->QueryInterface(IID_IDispatch, (void**)&pItem);
-
-		Item.vt = VT_DISPATCH;
-		Item.pdispVal = pItem;
-
-		Options.vt = VT_I4;
-		Options.lVal = 1024 | 512 | 16 | 4;//http://msdn.microsoft.com/en-us/library/bb787866(VS.85).aspx
-
-		bool retval = pDestination->CopyHere(Item, Options) == S_OK;
-
-		pItem->Release(); pItem = 0L;
-		pFilesInside->Release(); pFilesInside = 0L;
-		pDestination->Release(); pDestination = 0L;
-		pZippedFile->Release(); pZippedFile = 0L;
-		pISD->Release(); pISD = 0L;
-
-		return retval;
-
-	}
-	__finally
-	{
-		CoUninitialize();
-	}
-}
 
 std::string CurrentDirectory()
 {
@@ -112,7 +31,8 @@ std::string CurrentDirectory()
 	 CURL* curl;
 	 FILE* fp;
 	 CURLcode res;
-	 const char* url = "https://drive.google.com/file/d/11t3lfy6ut7lv4syr8gZRL4ptQ5eyQ9Ud/view?usp=sharing";
+
+	 const char* url = "";
 	 const char fileName[FILENAME_MAX] = "dl.zip";
 	 curl = curl_easy_init();
 	 if (curl) {
@@ -138,7 +58,7 @@ int main() {
 
 	DownloadFiles();
 	std::this_thread::sleep_for(100ms);
-	Unzip2Folder((BSTR)L"dl.zip", (BSTR)L"./");
+	//TODO: unzip dl.zip files
 	remove("dl.zip");
 	return 1;
 
